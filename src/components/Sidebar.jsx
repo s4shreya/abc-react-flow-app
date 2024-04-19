712746b75d566b92ce17aeddaaea29551d72779d
s4shreya <s4shreyadhingra@gmail.com>
2024-04-19 23:25:50 +0530

    added sidebar for more functionality

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,16 @@
-import styles from './Sidebar.module.css';
+import styles from "./Sidebar.module.css";
 
-const Sidebar = props => {
-
+const Sidebar = (props) => {
   return (
-    <div className={styles.container}>Sidebar
-    <button onClick={() => props.addNewNodeHandler("circle")}>Add circle</button>
-    <button onClick={() => props.addNewNodeHandler("rectangle")}>Add rectangle</button>
+    <div className={styles.container}>
+      Sidebar
+      <button onClick={() => props.addNewNodeHandler("circle")}>
+        Add circle
+      </button>
+      <button onClick={() => props.addNewNodeHandler("rectangle")}>
+        Add rectangle
+      </button>
     </div>
-  )
-}
-export default Sidebar
+  );
+};
+export default Sidebar;
